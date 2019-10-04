94d9d3a91374ad532f55a95bfdf4080bebfb16cf
ben attaya atef <atefbenattaya@gmail.com>
2019-10-04 13:36:31 +0200

    java interface done

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -21,4 +21,49 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
-}
+
+    @Override
+    public void takeOff() {
+        if (!this.flying && this.altitude == 0) {
+            this.flying = true;
+            System.out.printf("%s flew into the air.%n", this.getName());
+        }
+
+    }
+
+    @Override
+    public int ascend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.min(this.altitude + meters, 325);
+            System.out.printf("%s flew upward, altitude : %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+
+
+
+    @Override
+    public int descend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.max(this.altitude - meters, 0);
+            System.out.printf("%s flew downward, altitude : %d%n", this.getName(), this.altitude);
+        }
+          return this.altitude;
+    }
+
+    @Override
+    public void glide(){
+            System.out.println("It glides into the air ");
+        }
+
+     @Override
+     public void land(){
+         if (this.flying && this.altitude == 1) {
+             System.out.printf("%s land on earth.%n", this.getName());
+         } else {
+             System.out.printf("%s is too high, it can't lands.%n", this.getName());
+         }
+     }
+
+    }
+
---
+++ Fly.java
@@ -0,0 +1,14 @@
+import org.w3c.dom.ls.LSOutput;
+
+public interface Fly {
+
+    void takeOff();
+    int ascend(int meters);
+    int descend(int meters);
+    void land();
+    void glide();
+
+
+
+
+}
